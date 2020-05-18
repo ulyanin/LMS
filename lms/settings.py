036b35d65b2a1a7ddd7fe78ec6878325c85cd56b
Dmitry Ulyanin <ulyanin1997@gmail.com>
2020-05-18 05:31:59 +0300

    docker by defaukt

--- lms/settings.py
+++ lms/settings.py
@@ -4,7 +4,8 @@ POSTGRES_CONNECTION_CONF = {
     'user': 'admin',
     'password': 'admin',
     'database': 'lms_db',
-    'host': '127.0.0.1',
+    'host': 'lms_postgres_1',
+    # 'host': '127.0.0.1',
 }
 
 COOKIE_SECRET = 'secure_cookie'
